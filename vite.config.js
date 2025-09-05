5c13c9de3b354bf7a975d11420a0bd75345da58b
Zakaria Farih <zakaria.farih@digitaldatarealtime.com>
2025-09-05 20:14:34 +0200

    Use relative paths and remove custom vercel config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
